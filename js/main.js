8fe557df5ff430dfc31040b716aba7571495a2af
Claire Davis <cedavi23@ncsu.edu>
2021-06-06 13:38:01 -0400

    updating about me and formatting

--- js/main.js
+++ js/main.js
@@ -1,11 +1,2 @@
 'use strict'
 
-
-function logSubmit(event) {
-    log.textContent = `Form Submitted! Time stamp: ${event.timeStamp}`;
-    event.preventDefault();
-  }
-  
-  const form = document.getElementById('form');
-  const log = document.getElementById('log');
-  form.addEventListener('submit', logSubmit);
