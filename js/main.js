f4dee981f70e8c6a8546280fd88319b06afdce8d
Claire Davis <claireb1dav@gmail.com>
2021-06-07 13:24:04 -0400

    Adding dynamic navigation menu

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@
 function sideMenu() {
 	var side = document.getElementById("sidebar"),
 	about = document.getElementById("about");
-	side.style.width = side.style.width === "100px" ? '0' : '100px';
-	about.style.marginRight = about.style.marginRight === "100px" ? '0' : '100px';
+	side.style.width = side.style.width === "100px" ? '0px' : '100px';
+	about.style.marginRight = about.style.marginRight === "100px" ? '0px' : '100px';
 }
