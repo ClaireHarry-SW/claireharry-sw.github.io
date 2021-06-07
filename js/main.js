59f3eabeab486aeffafc91dd33092fd053ceff6c
Claire Davis <claireb1dav@gmail.com>
2021-06-07 14:22:17 -0400

    attempting to fix form

--- js/main.js
+++ js/main.js
@@ -6,3 +6,13 @@ function sideMenu() {
 	side.style.width = side.style.width === "100px" ? '0px' : '100px';
 	about.style.marginRight = about.style.marginRight === "100px" ? '0px' : '100px';
 }
+
+function saveForm() {
+	
+	document.cookie = "fname=" + escape(document.getElementById('fname').value) + ";";
+	document.cookie = "lname=" + escape(document.getElementById('lname').value) + ";";
+	document.cookie = "email=" + escape(document.getElementById('email').value) + ";";
+	document.cookie = "message=" + escape(document.getElementById('message').value) + ";";
+
+	document.getElementById('submission').innerHTML = "Message Submitted Successfully!";
+}
